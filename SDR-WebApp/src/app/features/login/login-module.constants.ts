ef9397a97c00c00c370c45eca26978e16afeefb4
Indu Sekhar Sajja <indu.sekhar.sajja@accenture.com>
2023-04-25 17:28:10 +0530

    Auth Token generation functionality added in Login page

--- SDR-WebApp/src/app/features/login/login-module.constants.ts
+++ SDR-WebApp/src/app/features/login/login-module.constants.ts
@@ -1,10 +1,16 @@
 import { LoginRoutingModule } from './login-routing-module';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { LoginComponent } from './components/login/login.component';
 export class LoginModuleConstants {
   static MODULE_COMPONENTS = [LoginComponent];
-  static MODULE_IMPORTS = [CommonModule, SharedModule, LoginRoutingModule];
+  static MODULE_IMPORTS = [
+    CommonModule,
+    SharedModule,
+    LoginRoutingModule,
+    ClipboardModule,
+  ];
   static MODULE_PROVIDERS = [];
   static MODULE_ENTRY_COMPONENTS = [];
 }
