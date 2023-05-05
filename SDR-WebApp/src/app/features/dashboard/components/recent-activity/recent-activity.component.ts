171cda6c4f8d1d2c6b55ab58c99494541fff5edf
nigama-swaminathan <nigama.swaminathan@accenture.com>
2023-05-05 13:36:17 +0530

    US683 - Enable local code steup to bypass authentication
    Added flag in environment settings to enable/disable features to deny/allow authentication.
    Cleaned up URL config and dynamically added APIM path if not runnign locally.
    Added dummy token related details on home page when auth is disabled.
    Enabled/disabled MSAL guard in routing module basedon newly added flag

--- SDR-WebApp/src/app/features/dashboard/components/recent-activity/recent-activity.component.ts
+++ SDR-WebApp/src/app/features/dashboard/components/recent-activity/recent-activity.component.ts
@@ -9,6 +9,7 @@ import { CommonMethodsService } from '../../../../shared/services/common-methods
 import { MsalBroadcastService } from '@azure/msal-angular';
 import { EventMessage, EventType } from '@azure/msal-browser';
 import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-recent-activity',
@@ -79,30 +80,38 @@ export class RecentActivityComponent {
    * To get 'homeAccountId' after login for silent logout
    */
   ngOnInit() {
-    this.msalBroadcastService.msalSubject$
-      .pipe(
-        filter(
-          (msg: EventMessage) =>
-            msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS
-        ),
-        takeUntil(this._destroying$)
-      )
-      .subscribe((result: any) => {
-        if (result?.payload?.accessToken) {
-          localStorage.setItem('token', result?.payload?.accessToken);
-          localStorage.setItem(
-            'homeAccountId',
-            result?.payload?.account?.homeAccountId
-          );
-          let isAdmin: any =
-            result?.payload?.account?.idTokenClaims?.roles?.indexOf(
-              'org.admin'
-            ) >= 0;
-          localStorage.setItem('isAdmin', isAdmin);
-          this.getVersions();
-        }
-        this.ds.changeDialogState('Home');
-      });
+    if (!environment.bypassAuth) {
+      this.msalBroadcastService.msalSubject$
+        .pipe(
+          filter(
+            (msg: EventMessage) =>
+              msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS
+          ),
+          takeUntil(this._destroying$)
+        )
+        .subscribe((result: any) => {
+          if (result?.payload?.accessToken) {
+            localStorage.setItem('token', result?.payload?.accessToken);
+            localStorage.setItem(
+              'homeAccountId',
+              result?.payload?.account?.homeAccountId
+            );
+            let isAdmin: any =
+              result?.payload?.account?.idTokenClaims?.roles?.indexOf(
+                'org.admin'
+              ) >= 0;
+            localStorage.setItem('isAdmin', isAdmin);
+            this.getVersions();
+          }
+          this.ds.changeDialogState('Home');
+        });
+    } else {
+      localStorage.setItem('isAdmin', 'true');
+      localStorage.setItem('token', 'DUMMY_TOKEN');
+      localStorage.setItem('homeAccountId', 'DUMMY_ACCOUNT_ID');
+      this.getVersions();
+      this.ds.changeDialogState('Home');
+    }
   }
 
   /**
--- SDR-WebApp/src/app/features/login/components/login/login.component.ts
+++ SDR-WebApp/src/app/features/login/components/login/login.component.ts
@@ -1,5 +1,5 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, Inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import {
   MsalService,
   MSAL_GUARD_CONFIG,
@@ -13,6 +13,7 @@ import {
   RedirectRequest,
 } from '@azure/msal-browser';
 import { configList } from 'src/app/shared/components/study-element-description/config/study-element-field-config';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -24,52 +25,66 @@ export class LoginComponent {
   authToken: string;
   constructor(
     @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
-    private authService: MsalService
+    private authService: MsalService,
+    public router: Router,
+    public activatedRoute: ActivatedRoute
   ) {}
   ngOnInit() {
-    this.authService.logoutRedirect({
-      onRedirectNavigate: () => {
-        // Return false to stop navigation after local logout
-        return false;
-      },
-    });
+    if (!environment.bypassAuth) {
+      this.authService.logoutRedirect({
+        onRedirectNavigate: () => {
+          // Return false to stop navigation after local logout
+          return false;
+        },
+      });
+    }
   }
   /**
    *Login on click of link
    */
   login() {
-    if (this.msalGuardConfig.authRequest) {
-      this.authService.loginRedirect({
-        ...this.msalGuardConfig.authRequest,
-      } as RedirectRequest);
+    if (!environment.bypassAuth) {
+      if (this.msalGuardConfig.authRequest) {
+        this.authService.loginRedirect({
+          ...this.msalGuardConfig.authRequest,
+        } as RedirectRequest);
+      } else {
+        this.authService.loginRedirect();
+      }
     } else {
-      this.authService.loginRedirect();
+      this.router.navigate(['home'], { relativeTo: this.activatedRoute });
     }
   }
 
   getToken() {
-    if (this.msalGuardConfig.authRequest) {
-      this.msalGuardConfig.interactionType = InteractionType.Popup;
-      var request = this.msalGuardConfig.authRequest as PopupRequest;
-      this.authService.acquireTokenPopup(request).subscribe({
-        next: (result: AuthenticationResult) => {
-          this.showError = false;
-          this.authToken = result.accessToken;
-        },
-        error: (error) => {
-          const authErr = error as AuthError;
-          if (authErr) {
-            if (authErr.errorCode == configList.USER_CANCELLED_ERROR_CODE) {
-              this.showError = false;
-            } else if (authErr.errorCode == configList.INTERACTION_ERROR_CODE) {
-              alert(configList.AUTH_IN_PROGRESS_ERROR);
-              this.showError = false;
-            } else {
-              this.showError = true;
+    if (!environment.bypassAuth) {
+      if (this.msalGuardConfig.authRequest) {
+        this.msalGuardConfig.interactionType = InteractionType.Popup;
+        var request = this.msalGuardConfig.authRequest as PopupRequest;
+        this.authService.acquireTokenPopup(request).subscribe({
+          next: (result: AuthenticationResult) => {
+            this.showError = false;
+            this.authToken = result.accessToken;
+          },
+          error: (error) => {
+            const authErr = error as AuthError;
+            if (authErr) {
+              if (authErr.errorCode == configList.USER_CANCELLED_ERROR_CODE) {
+                this.showError = false;
+              } else if (
+                authErr.errorCode == configList.INTERACTION_ERROR_CODE
+              ) {
+                alert(configList.AUTH_IN_PROGRESS_ERROR);
+                this.showError = false;
+              } else {
+                this.showError = true;
+              }
             }
-          }
-        },
-      });
+          },
+        });
+      }
+    } else {
+      alert('Authentication is disabled.');
     }
   }
 }
