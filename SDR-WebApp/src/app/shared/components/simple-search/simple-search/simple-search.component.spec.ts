bf4b54386a6bd57a2e59752cec0029136c5c13fe
swapna-katturi <104914077+swapna-katturi@users.noreply.github.com>
2023-07-05 17:27:08 +0530

    added new unit tests

--- SDR-WebApp/src/app/shared/components/simple-search/simple-search/simple-search.component.spec.ts
+++ SDR-WebApp/src/app/shared/components/simple-search/simple-search/simple-search.component.spec.ts
@@ -271,4 +271,16 @@ describe('SimpleSearchComponent', () => {
       expect(val).not.toEqual('');
     });
   });
+  it('submitSearch makes error calls', () => {
+    spyOn(window, 'alert');
+    component.showGrid = true;
+    component.editorForm.patchValue({
+      fromDate: '12-08-2023',
+      toDate: '12-05-2023',
+    });
+    component.submitSearch();
+    expect(window.alert).toHaveBeenCalledWith(
+      'To Date must be greater than From Date'
+    );
+  });
 });
