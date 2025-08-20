89850aa568226a7cab7c3792cc59b76d8d391b1d
Jon Masson <jonathan.masson@capgemini.com>
2025-08-20 08:47:58 +0100

    Remove MSAL Guard and Interceptor config

--- SDR-WebApp/src/app/app-module.constants.ts
+++ SDR-WebApp/src/app/app-module.constants.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import {
-  InteractionType,
-} from '@azure/msal-browser';
-import {
   MsalModule,
   MsalGuard,
   MsalInterceptor,
   MsalBroadcastService,
-  MsalInterceptorConfiguration,
   MsalService,
-  MSAL_GUARD_CONFIG,
-  MSAL_INTERCEPTOR_CONFIG,
-  MsalGuardConfiguration,
-} from '@azure/msal-angular'; // Import MsalInterceptor
+} from '@azure/msal-angular';
 import { environment } from '../environments/environment';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -24,6 +17,7 @@ import { AgGridModule } from 'ag-grid-angular';
 import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
 import { SoaComponent } from './features/soa/soa.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+
 export class AppModuleConstants {
   static MODULE_IMPORTS = [
     BrowserAnimationsModule,
@@ -43,14 +37,6 @@ export class AppModuleConstants {
       useClass: MsalInterceptor,
       multi: true,
     },
-    {
-      provide: MSAL_GUARD_CONFIG,
-      useFactory: MSALGuardConfigFactory,
-    },
-    {
-      provide: MSAL_INTERCEPTOR_CONFIG,
-      useFactory: MSALInterceptorConfigFactory,
-    },
     MsalGuard,
     MsalService,
     MsalBroadcastService,
@@ -67,30 +53,3 @@ export const protectedResources = {
     //scopes: ["api://Enter_the_Application_Id_of_Service_Here/.default"],
   },
 };
-// export const loginRequest = {
-//   scopes: [...protectedResources.profileApi.scopes]
-// };
-
-export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
-  const protectedResourceMap = new Map<string, Array<string>>();
-  if (!environment.bypassAuth) {
-    protectedResourceMap.set(
-      protectedResources.profileApi.endpoint,
-      protectedResources.profileApi.scopes
-    );
-  }
-
-  return {
-    interactionType: InteractionType.Redirect,
-    protectedResourceMap,
-  };
-}
-
-export function MSALGuardConfigFactory(): MsalGuardConfiguration {
-  return {
-    interactionType: InteractionType.Redirect,
-    authRequest: {
-      scopes: [environment.Audience],
-    },
-  };
-}
