d95d236ab699567e91597f25c621e7012daa7b21
Jon Masson <jonathan.masson@capgemini.com>
2025-08-20 08:30:59 +0100

    Remove guards from routes and redirect '' to 'home' (skip login page)

--- SDR-WebApp/src/app/app-routing.module.ts
+++ SDR-WebApp/src/app/app-routing.module.ts
@@ -2,23 +2,18 @@ import { NgModule } from '@angular/core';
 import {
   RouterModule,
   Routes,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
 } from '@angular/router';
-import { MsalGuard } from '@azure/msal-angular'; // Import MsalInterceptor
-import { AuthGuardService } from './auth-guard.service';
-import { environment } from 'src/environments/environment';
+
 
 const routes: Routes = [
   {
     path: '',
     canActivate: [],
     children: [
-      {
+       {
         path: '',
-        loadChildren: () =>
-          import('./features/login/login.module').then((x) => x.LoginModule),
-        canActivate: [],
+        redirectTo: 'home',
+        pathMatch: 'full'
       },
       {
         path: 'home',
@@ -26,7 +21,7 @@ const routes: Routes = [
           import('./features/dashboard/dashbord.module').then(
             (x) => x.DashboardModule
           ),
-        canActivate: environment.bypassAuth ? [] : [MsalGuard],
+        canActivate: []
       },
       {
         path: 'search',
@@ -34,7 +29,7 @@ const routes: Routes = [
           import('./features/search-study/search-study.module').then(
             (x) => x.SearchStudyModule
           ),
-        canActivate: environment.bypassAuth ? [] : [MsalGuard],
+        canActivate: []
       },
       {
         path: 'comparison',
@@ -42,14 +37,14 @@ const routes: Routes = [
           import('./features/study-compare/study-compare.module').then(
             (x) => x.StudyCompareModule
           ),
-        canActivate: environment.bypassAuth ? [] : [MsalGuard],
+        canActivate: []
       },
       {
         path: 'admin',
         loadChildren: () =>
           import('./features/admin/admin.module').then((x) => x.AdminModule),
-        canActivate: environment.bypassAuth ? [] : [MsalGuard],
-        canLoad: environment.bypassAuth ? [] : [AuthGuardService],
+        canActivate: [],
+        canLoad: []
       },
       {
         path: 'reports',
@@ -57,8 +52,8 @@ const routes: Routes = [
           import('./features/reports/reports.module').then(
             (x) => x.ReportsModule
           ),
-        canActivate: environment.bypassAuth ? [] : [MsalGuard],
-        canLoad: environment.bypassAuth ? [] : [AuthGuardService],
+        canActivate: [],
+        canLoad: []
       },
     ],
   },
