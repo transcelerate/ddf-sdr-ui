46ef81ea9555cf94f0d5950483fbb50f8da4ba58
Jon Masson <jonathan.masson@capgemini.com>
2025-08-27 07:49:21 +0100

    Remove unused import

--- SDR-WebApp/src/app/app-module.constants.ts
+++ SDR-WebApp/src/app/app-module.constants.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { environment } from '../environments/environment';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
